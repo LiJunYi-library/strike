154940f048862474c40c5942d2740ba205a02eaa
13588105883 <1040381245@qq.com>
2023-08-22 15:59:44 +0800

    增加listLoad hook

--- v-hooks/package/list/listLoad.js
+++ v-hooks/package/list/listLoad.js
@@ -13,12 +13,15 @@ function getListLoadProps(options) {
     setTotal: (res) => {
       if (!res) return 0;
       if (res instanceof Array) return res.length;
-      return res.total;
+      return res.total * 1;
     },
-    setFinished: (list, total, res) => {
-      return list.length >= total;
+    setFinished: (res, hooks) => {
+      return hooks.list.length >= hooks.total;
     },
     fetchCB: () => undefined,
+    fetchBegainCB: () => undefined,
+    fetchConcatCB: () => undefined,
+    beforeBegain: () => undefined,
     currentPage: 1,
     pageSize: 10,
     accumulationList: true,
@@ -32,15 +35,65 @@ function getListLoadProps(options) {
 
 function useListLoad(props = {}) {
   const config = getListLoadProps(props);
-
   const asyncHooks = usePromise(config.fetchCb, {
     ...config,
-    then: (data) => {
-      radioHooks.updateListToResolveValue(data);
-    },
+    then: (data) => {},
   });
 
-  const arguments_ = { ...asyncHooks };
-  arguments_.proxy = reactive(arguments_);
+  const list = ref(config.list);
+  const currentPage = ref(config.currentPage);
+  const pageSize = ref(config.pageSize);
+  const total = ref(0);
+  const finished = ref(false);
+  const listData = ref([]);
+
+  const arguments_ = {
+    ...asyncHooks,
+    list,
+    listData,
+    currentPage,
+    pageSize,
+    finished,
+    total,
+    fetchBegain,
+    fetchConcat,
+  };
+  const proxy = reactive(arguments_);
+  arguments_.proxy = proxy;
+
+  function fetchBegain(...arg) {
+    list.value = [];
+    listData.value = [];
+    currentPage.value = 1;
+    finished.value = false;
+    total.value = 0;
+    config.beforeBegain(proxy);
+    return asyncHooks.runBegain(...arg).then((res) => {
+      listData.value = config.setList(res, proxy);
+      list.value = listData.value;
+      currentPage.value = currentPage.value + 1;
+      total.value = config.setTotal(res, proxy);
+      finished.value = config.setFinished(res, proxy);
+      config.fetchBegainCB(proxy);
+    });
+  }
+
+  function fetchConcat(...arg) {
+    if (finished.value === true) {
+      return;
+    }
+    if (asyncHooks.loading.value === true) {
+      return;
+    }
+    return asyncHooks.run(...arg).then((res) => {
+      listData.value = config.setList(res, proxy);
+      list.value = list.value.concat(listData.value);
+      currentPage.value = currentPage.value + 1;
+      total.value = config.setTotal(res, proxy);
+      finished.value = config.setFinished(res, proxy);
+      config.fetchConcatCB(proxy);
+    });
+  }
+
   return arguments_;
 }
--- v-hooks/package/list/radio.js
+++ v-hooks/package/list/radio.js
@@ -93,6 +93,7 @@ function useRadio(props = {}) {
     restore,
     onSelect,
     same,
+    reset,
     formatterValue,
     formatterLabel,
     updateList,

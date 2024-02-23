a7e410a2e6b3593db9887db3840011443b7f1f72
13588105883 <1040381245@qq.com>
2024-02-23 10:01:56 +0800

    useSessionStorageRef

--- v-hooks/package/browser/cache/sessionStorage.js
+++ v-hooks/package/browser/cache/sessionStorage.js
@@ -12,40 +12,34 @@ import {
   isRef,
 } from "vue";
 
-// export function useSessionStorageRef(key, defaultValue) {
-//   window.addEventListener("storage", onStorageChange);
-//   const storageStr = window.sessionStorage.getItem(key);
-//   const storageVal = (() => {
-//     if (storageStr === "undefined") return undefined;
-//     if (storageStr === null) {
-//       if (defaultValue !== undefined) {
-//         window.sessionStorage.setItem(key, JSON.stringify(defaultValue));
-//       }
-//       return defaultValue;
-//     }
-//     return JSON.parse(storageStr);
-//   })();
 
-//   const val = ref(storageVal);
+export function useSessionStorageRef(key, defaultValue, props = {}) {
+  const config = { isListener: true, onChange: () => undefined, ...props };
 
-//   function onStorageChange(event) {
-//     if (event.key !== key) return;
-//     const str = window.sessionStorage.getItem(key);
-//     const sv = (() => {
-//       if (str === "undefined") return undefined;
-//       return JSON.parse(str);
-//     })();
-//     val.value = sv;
-//   }
+  const storageStr = window.sessionStorage.getItem(key);
+  const storageVal = (() => {
+    if (storageStr === "undefined") return undefined;
+    if (storageStr === null) {
+      if (defaultValue !== undefined) {
+        window.sessionStorage.setItem(key, JSON.stringify(defaultValue));
+      }
+      return defaultValue;
+    }
+    return JSON.parse(storageStr);
+  })();
 
-//   watch(
-//     val,
-//     (newValue) => {
-//       const newVal = isRef(newValue) ? newValue.value : newValue;
-//       window.sessionStorage.setItem(key, JSON.stringify(newVal));
-//     },
-//     { deep: true }
-//   );
+  const val = ref(storageVal);
 
-//   return val;
-// }
+  watch(
+    val,
+    (newValue) => {
+      const newVal = isRef(newValue) ? newValue.value : newValue;
+      window.sessionStorage.setItem(key, JSON.stringify(newVal));
+    },
+    { deep: true }
+  );
+
+  return val;
+}
+
+

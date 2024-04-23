d2823cfec0f9750a0cdedd03fea02b31e725a386
13588105883 <1040381245@qq.com>
2024-04-23 13:36:28 +0800

    解决版本冲突

--- vue/v-view/components/index.d.ts
+++ vue/v-view/components/index.d.ts
@@ -1,19 +1,19 @@
-// export * from "./carousel/carousel";
-// export * from "./tab/tab";
-// export * from "./virtual-list/list";
-// export * from "./virtual-list/list-falls";
-// export * from "./view-page";
-// export * from "./page-view";
-// export * from "./icon";
-// export * from './loading'
-// export * from './scroll'
-// export * from './layout'
-// export * from './dropdown'
-// export * from './popup'
-// export * from './pulldown'
-// export * from './list'
-// export * from './img'
-// export * from './resize'
+export * from "./carousel/carousel";
+export * from "./tab/tab";
+export * from "./virtual-list/list";
+export * from "./virtual-list/list-falls";
+export * from "./view-page";
+export * from "./page-view";
+export * from "./icon";
+export * from './loading'
+export * from './scroll'
+export * from './layout'
+export * from './dropdown'
+export * from './popup'
+export * from './pulldown'
+export * from './list'
+export * from './img'
+export * from './resize'
 export * from './grid'
-// export * from './toast'
-// export * from './chart'
+export * from './toast'
+export * from './chart'

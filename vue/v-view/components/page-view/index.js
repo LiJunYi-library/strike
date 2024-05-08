3a7a1d251ea48df9b247dfeb8b51ab54208ac49d
13588105883 <1040381245@qq.com>
2024-05-08 21:00:35 +0800

    text组件

--- vue/v-view/components/page-view/index.js
+++ vue/v-view/components/page-view/index.js
@@ -8,14 +8,85 @@ import {
   provide,
   inject,
   onBeforeUnmount,
-  watch,
+  watchEffect,
   ref,
 } from "vue";
 import "./index.scss";
 
+const TAP_OFFSET = 5;
+
+function getDirection(x, y) {
+  if (x > y) {
+    return "horizontal";
+  }
+  if (y > x) {
+    return "vertical";
+  }
+  return "";
+}
+
+function useTouch() {
+  const startX = ref(0);
+  const startY = ref(0);
+  const deltaX = ref(0);
+  const deltaY = ref(0);
+  const offsetX = ref(0);
+  const offsetY = ref(0);
+  const direction = ref("");
+  const isTap = ref(true);
+  const isVertical = () => direction.value === "vertical";
+  const isHorizontal = () => direction.value === "horizontal";
+  const reset = () => {
+    deltaX.value = 0;
+    deltaY.value = 0;
+    offsetX.value = 0;
+    offsetY.value = 0;
+    direction.value = "";
+    isTap.value = true;
+  };
+  const start = (event) => {
+    reset();
+    startX.value = event.touches[0].clientX;
+    startY.value = event.touches[0].clientY;
+  };
+  const move = (event) => {
+    const touch = event.touches[0];
+    deltaX.value = (touch.clientX < 0 ? 0 : touch.clientX) - startX.value;
+    deltaY.value = touch.clientY - startY.value;
+    offsetX.value = Math.abs(deltaX.value);
+    offsetY.value = Math.abs(deltaY.value);
+    const LOCK_DIRECTION_DISTANCE = 10;
+    if (
+      !direction.value ||
+      (offsetX.value < LOCK_DIRECTION_DISTANCE && offsetY.value < LOCK_DIRECTION_DISTANCE)
+    ) {
+      direction.value = getDirection(offsetX.value, offsetY.value);
+    }
+    if (isTap.value && (offsetX.value > TAP_OFFSET || offsetY.value > TAP_OFFSET)) {
+      isTap.value = false;
+    }
+  };
+  return {
+    move,
+    start,
+    reset,
+    startX,
+    startY,
+    deltaX,
+    deltaY,
+    offsetX,
+    offsetY,
+    direction,
+    isVertical,
+    isHorizontal,
+    isTap,
+  };
+}
+
 const props = {
   listHook: Object,
   lazy: Boolean,
+  flex: { type: Boolean, default: false },
   cache: { type: Boolean, default: true },
   width: { type: Number, default: window.innerWidth },
   behavior: { type: String, default: "smooth" }, // smooth  instant
@@ -24,69 +95,123 @@ const props = {
 const Context = defineComponent({
   props,
   setup(props, context) {
-    const itemsHtml = [];
-    let parentHtml = null;
-    let containerHtml = null;
-    let prveSLeft = 0;
-    let sLeft = 0;
-    let lock = false;
-    let isTriggerWatch = true;
-    const actItemHtml = null;
-    const RPageViewContext = inject("RPageViewContext") || {};
+    const touch = useTouch();
+    const delta = computed(() => touch.deltaX.value || 0);
 
-    onMounted(() => {
-      //
+    const RPageViewContext = inject("RPageViewContext") || {};
+    const containerHtml = ref();
+    const actHtml = ref();
+    const width = computed(() => containerHtml.value?.getBoundingClientRect?.()?.width);
+    const height = computed(() => (props.flex ? "" : actHtml.value?.offsetHeight));
+    const parentW = computed(() => width.value * (props.listHook?.list?.length ?? 0) + "px");
+    const translateX = ref(width.value * -props.listHook.index + delta.value);
+    const isUseHook = computed(() => !RPageViewContext?.children?.length);
+    const listRenderData = computed(() =>
+      isUseHook.value ? props.listHook.list : RPageViewContext.children
+    );
+    const minTranslateX = computed(() => (listRenderData.value.length - 1) * -width.value);
+    const maxTranslateX = computed(() => 0);
+    let parentHtml = null;
+    const itemsHtml = [];
+    const isTransition = ref(false);
+    const disableSwipHorizontal = computed(() => {
+      const x = translateX.value + touch.deltaX.value;
+      if (x > maxTranslateX.value) return true;
+      if (x < minTranslateX.value) return true;
+      return false;
     });
 
-    watch(
-      () => props.listHook.select,
-      () => {
-        if (isTriggerWatch === false) {
-          isTriggerWatch = true;
-          return;
-        }
-        lock = true;
-        // console.log("watch");
-        scrollTo();
-      }
-    );
+    onMounted(() => {
+      isTransition.value = false;
+    });
 
-    function scrollTo() {
-      const left = props.width * props.listHook.index;
-      containerHtml.scrollTo({ left, behavior: props.behavior });
+    watchEffect(() => {
+      console.log("watchEffect");
+      translateX.value = width.value * -props.listHook.index;
+      isTransition.value = true;
+      setTranslateX(width.value * -props.listHook.index);
+    });
+
+    function setTranslateX(x) {
+      if (!parentHtml) return;
+      parentHtml.style.transform = `translateX(${x}px)`;
     }
 
-    function onTouchStart() {
-      // console.log("onTouchStart");
+    function onTransitionEnd(event) {
+      console.log("onTransitionEnd");
+      isTransition.value = false;
+    }
+
+    function onTouchStart(event) {
+      // let tou = event.touches[0];
+      // var pressure = tou.force / tou.radiusX / tou.radiusY;
+      // console.log(" onTouchStart tou.force", tou.force, pressure);
+      isTransition.value = false;
+      touch.start(event);
+    }
+
+    function onTouchMove(event) {
+      // let tou = event.touches[0];
+      // var pressure = tou.force / tou.radiusX / tou.radiusY;
+      // console.log(" onTouchMove tou.force", event);
+      // console.log(" onTouchMove tou.force", tou.force, tou.maxForce);
+      touch.move(event);
+      if (touch.isHorizontal()) onSwipHorizontal(event);
+    }
+
+    function onTouchEnd(event) {
+      // let tou = event.touches[0];
+      // console.log("onTouchEnd tou.force", event);
+      if (touch.isHorizontal()) onSwipHorizontalEnd(event);
+    }
+
+    function onSwipHorizontal(event) {
+      if (disableSwipHorizontal.value) return;
+      event.stopPropagation();
+      event.preventDefault();
+      setTranslateX(translateX.value + touch.deltaX.value);
+    }
+
+    function onSwipHorizontalEnd(event) {
+      if (disableSwipHorizontal.value) return;
+      event.stopPropagation();
+      event.preventDefault();
+      if (touch.deltaX.value < 0) onSwipLeft();
+      if (touch.deltaX.value > 0) onSwipRight();
+    }
+
+    function onSwipLeft() {
+      isTransition.value = true;
+      if (touch.offsetX.value > width.value / 3) {
+        const index = props.listHook.index + 1;
+        if (index >= props.listHook?.list?.length) return;
+        props?.listHook?.updateIndex(index);
+      } else {
+        setTranslateX(translateX.value);
+      }
+    }
+
+    function onSwipRight() {
+      isTransition.value = true;
+      if (touch.offsetX.value > width.value / 3) {
+        const index = props.listHook.index - 1;
+        if (index < 0) return;
+        props?.listHook?.updateIndex(index);
+      } else {
+        setTranslateX(translateX.value);
+      }
     }
 
     function onScrollend(params) {
-      // console.log("onScrollend");
-      lock = false;
+      //
     }
 
     function update(index) {
-      // console.log("update",lock);
-      if (index === props.listHook.index) return;
-      if (lock === true) return;
-      isTriggerWatch = false;
-      props.listHook.updateIndex(index);
-      context.emit("change", index);
+      //
     }
 
     function onScroll(event) {
-      // console.log("onScroll");
-      sLeft = containerHtml.scrollLeft;
-      const sapce = sLeft - prveSLeft;
-      if (sapce < 0) onScrollRight(event, sLeft);
-      if (sapce > 0) onScrollLeft(event, sLeft);
-      prveSLeft = sLeft;
-
-      const index = Math.round(sLeft) / props.width;
-      if (!Number.isInteger(index)) return;
-      // console.log(index, props.listHook.index);
-      update(index);
-      lock = false;
+      //
     }
 
     function onScrollLeft(event, sLeft) {
@@ -97,52 +222,71 @@ const Context = defineComponent({
       //
     }
 
+    function getItemsHtml(el, same, item, index) {
+      itemsHtml[index] = el;
+      if (same(item, index)) actHtml.value = el;
+    }
+
+    function renderItem(isUseHook, same, item, index) {
+      if (Math.abs(props.listHook.index - index) > 1) return null;
+      return isUseHook
+        ? renderSlot(RPageViewContext.slots, "item", { item, index })
+        : renderSlot(item.slots, "default");
+    }
+
     function renderContent() {
-      const isUseHook = !RPageViewContext?.children?.length;
-      const listRenderData = isUseHook ? props.listHook.list : RPageViewContext.children;
       const same = (item, index) => {
-        if (isUseHook) return props.listHook.same(item);
+        if (isUseHook.value) return props.listHook.same(item);
         return props.listHook.index === index;
       };
 
-      return renderList(listRenderData, (item, index) => {
-
+      return renderList(listRenderData.value, (item, index) => {
         return (
           <div
-            style={{ width: props.width + "px" }}
+            style={{ width: width.value + "px" }}
             key={index}
-            ref={(el) => (itemsHtml[index] = el)}
+            ref={(el) => getItemsHtml(el, same, item, index)}
             class={["r-page-view-item", same(item, index) && "r-page-view-item-same"]}
           >
-            {isUseHook
-              ? renderSlot(RPageViewContext.slots, "item", { item, index })
-              : renderSlot(item.slots, "default")}
+            {renderItem(isUseHook.value, same, item, index)}
           </div>
         );
       });
     }
 
     return (vm) => {
+      // console.log("rrrrrrrrrrrrrr");
+      //  onTouchmove={onTouchMove}
+      // onTouchstart={onTouchStart}
+      // onScroll={onScroll}
+      // onScrollend={onScrollend}
       return (
         <div
           class={["r-page-view"]}
-          ref={(el) => (containerHtml = el)}
-          onScroll={onScroll}
-          onScrollend={onScrollend}
+          ref={(el) => (containerHtml.value = el)}
           onTouchstart={onTouchStart}
+          onTouchmove={onTouchMove}
+          onTouchend={onTouchEnd}
+          onTouchcancel={onTouchEnd}
         >
-          <div
-            ref={(el) => (parentHtml = el)}
-            class={["r-page-view-list"]}
-            style={{
-              width: props.width * (props.listHook?.list?.length ?? 0) + "px",
-            }}
-          >
-            {renderContent()}
+          <div class={["r-page-view-win"]} style={{ height: height.value + "px" }}>
+            <div
+              ref={(el) => (parentHtml = el)}
+              onTransitionend={onTransitionEnd}
+              class={[
+                "r-page-view-list",
+                isTransition.value && "r-page-view-list-transition",
+                props.flex && "r-page-view-list-flex",
+              ]}
+              style={{ width: parentW.value }}
+            >
+              {renderContent()}
+            </div>
           </div>
         </div>
       );
     };
+    //  transform: `translateX(${translateX.value}px)`
   },
 });
 
---
+++ vue/v-view/components/text/index.d.ts
@@ -0,0 +1,3 @@
+import { DefineComponent } from "vue";
+
+export declare const RText: DefineComponent<any>;

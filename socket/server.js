b23b7e61c1b99e9faedf893ab84e8f3e292d571e
lijunyi <1040381245@qq.com>
2024-09-11 09:39:33 +0800

    请求

---
+++ socket/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', function connection(ws) {
+  console.log('Server connection');
+  ws.on('message', function incoming(message) {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
+
+server.listen(4567, function listening() {
+  console.log('Server started on :4567');
+});
